29559011d513ccdbf53f81d2cd86c35d7d6a3e7a
Brandon Squizzato <bsquizza@redhat.com>
2018-11-12 16:12:14 -0500

    Tweak var names

--- vars/pipelineVars.groovy
+++ vars/pipelineVars.groovy
@@ -5,10 +5,19 @@ class pipelineVars implements Serializable {
     String swaggerContext = "continuous-integration/jenkins/swagger"
     String coverageContext = "continuous-integration/jenkins/coverage"
     String smokeContext = "continuous-integration/jenkins/e2e-smoke"
+
     String jenkinsSvcAccount = "jenkins"
-    String userPath = "~/.local/bin"
     String defaultNameSpace = "jenkins"
-    String gitSshCreds = "insightsdroid-ssh-git"
-    String smokeTestResourceLabel = "smoke_test_projects"
     String defaultNodeImage = "docker-registry.default.svc:5000/jenkins/jenkins-slave-base-centos7-python36:latest"
+
+    String gitSshCreds = "insightsdroid-ssh-git"
+
+    String userPath = "~/.local/bin"
+    String venvDir = "~/.venv"
+
+    String smokeTestResourceLabel = "smoke_test_projects"
+    String e2eDeployDir = 'e2e-deploy'
+    String e2eDeployRepo = 'https://github.com/RedHatInsights/e2e-deploy.git'
+    String e2eTestsDir = 'e2e-tests'
+    String e2eTestsRepo = 'https://github.com/RedHatInsights/e2e-tests.git'
 }
