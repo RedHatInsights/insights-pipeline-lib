a1710fd36d420f43ae0a71c0a238d0be305240f3
Brandon Squizzato <bsquizza@redhat.com>
2018-11-14 16:00:26 -0500

    Convert lint results to junit outside the try/except

--- vars/runPythonLintCheck.groovy
+++ vars/runPythonLintCheck.groovy
@@ -9,11 +9,13 @@ def call(parameters = [:]) {
             sh "${pipelineVars.userPath}/pipenv run python -m pytest --pylama --junitxml=lint-results.xml --ignore=tests/"
         } else {
             sh "${pipelineVars.userPath}/pipenv run flake8 . --output-file lint-results.txt"
-            sh "${pipelineVars.userPath}/pipenv run flake8_junit lint-results.txt lint-results.xml"
         }
     }
 
     try {
+        if (fileExists("lint-results.txt")) {
+            sh "${pipelineVars.userPath}/pipenv run flake8_junit lint-results.txt lint-results.xml"
+        }
         junit 'lint-results.xml'
     } catch (evalErr) {
         // allow the unit tests to run even if evaluating lint results failed...
