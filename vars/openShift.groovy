8c9042829dfb04f3cc8f4dae2f481d1c2424c2b5
Brandon Squizzato <bsquizza@redhat.com>
2018-11-20 11:30:08 -0500

    Override resource params if desired

--- vars/openShift.groovy
+++ vars/openShift.groovy
@@ -4,6 +4,10 @@
 def withNode(Map parameters = [:], Closure body = null) {
     image = parameters.get('image', pipelineVars.defaultNodeImage)
     namespace = parameters.get('namespace', pipelineVars.defaultNameSpace)
+    requestCpu = parameters.get('resourceRequestCpu', "200m")
+    limitCpu = parameters.get('resourceLimitCpu', "500m")
+    requestMemory = parameters.get('resourceRequestMemory', "256Mi")
+    limitMemory = parameters.get('resourceLimitMemory', "650Mi")
 
     label = "test-${UUID.randomUUID().toString()}"
 
@@ -19,10 +23,10 @@ def withNode(Map parameters = [:], Closure body = null) {
                 image: image,
                 alwaysPullImage: true,
                 args: '${computer.jnlpmac} ${computer.name}',
-                resourceRequestCpu: '200m',
-                resourceLimitCpu: '500m',
-                resourceRequestMemory: '256Mi',
-                resourceLimitMemory: '650Mi',
+                resourceRequestCpu: requestCpu,
+                resourceLimitCpu: limitCpu,
+                resourceRequestMemory: requestMemory,
+                resourceLimitMemory: limitMemory,
                 envVars: [
                     envVar(key: 'LC_ALL', value: 'en_US.utf-8'),
                     envVar(key: 'LANG', value: 'en_US.utf-8'),
