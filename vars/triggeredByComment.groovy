c169d5a9800231941c2f40109c74b753835691ad
Brandon Squizzato <bsquizza@redhat.com>
2018-12-18 20:37:20 -0500

    Also allow 'rebuild' to count when checking issue comment trigger

--- vars/triggeredByComment.groovy
+++ vars/triggeredByComment.groovy
@@ -3,11 +3,12 @@ import com.cloudbees.groovy.cps.NonCPS
 
 @NonCPS
 def triggeredByComment(currentBuild) {
-    def cause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause)
-    if (cause) {
+    def issueCommentCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause)
+    def rebuildCause = currentBuild.rawBuild.getCause(com.sonyericsson.rebuild.RebuildCause)
+    if (issueCommentCause || rebuildCause) {
         return true
     } else {
-        echo('Build not started by issue comment trigger')
+        echo('Build not started by issue comment trigger or rebuild trigger')
         return false
     }
 }
