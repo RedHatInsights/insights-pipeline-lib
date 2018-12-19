987d0744879b1aa988ad33ea1f2688cce64010b1
Brandon Squizzato <bsquizza@redhat.com>
2018-12-19 18:29:46 -0500

    Try gpg2

--- vars/runSmokeTest.groovy
+++ vars/runSmokeTest.groovy
@@ -82,7 +82,7 @@ private def deployEnvironment(refspec, project, helmComponentChartName, helmSmok
 
             // Decrypt the secrets config
             withCredentials([string(credentialsId: 'helm-secrets-gpg-shared-key', variable: 'PASSPHRASE')]) {
-                sh 'gpg --decrypt --out secrets.yaml --passphrase $PASSPHRASE secrets.yaml.gpg'
+                sh 'gpg2 --decrypt --out secrets.yaml --passphrase $PASSPHRASE secrets.yaml.gpg'
             }
 
             // Install the smoke test chart
