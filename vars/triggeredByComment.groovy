241d9f057bc6b784319c0c07c1515d385946f22d
Brandon Squizzato <bsquizza@redhat.com>
2018-12-18 20:48:04 -0500

    Support replay trigger too

--- vars/triggeredByComment.groovy
+++ vars/triggeredByComment.groovy
@@ -3,12 +3,13 @@ import com.cloudbees.groovy.cps.NonCPS
 
 @NonCPS
 def triggeredByComment(currentBuild) {
-    def issueCommentCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause)
-    def rebuildCause = currentBuild.rawBuild.getCause(com.sonyericsson.rebuild.RebuildCause)
-    if (issueCommentCause || rebuildCause) {
+    def issueCommentCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.pipeline.github.trigger.IssueCommentCause) != null
+    def rebuildCause = currentBuild.rawBuild.getCause(com.sonyericsson.rebuild.RebuildCause) != null
+    def replayCause = currentBuild.rawBuild.getCause(org.jenkinsci.plugins.workflow.cps.replay.ReplayCause) != null
+    if (issueCommentCause || rebuildCause || replayCause) {
         return true
     } else {
-        echo('Build not started by issue comment trigger or rebuild trigger')
+        echo('Build not started by issue comment trigger or rebuild/replay trigger')
         return false
     }
 }
