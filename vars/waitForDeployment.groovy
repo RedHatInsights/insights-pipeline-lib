d58406bb035b842f2a8444cc501f7f94fa5113ae
Dmitrii Misharov <dmisharo@redhat.com>
2019-03-07 09:53:42 +0100

    Added debug info

--- vars/waitForDeployment.groovy
+++ vars/waitForDeployment.groovy
@@ -4,30 +4,44 @@
  *  value. E.g. "app: compliance-backend".
  */
 
-def call(Integer minutes, String label, String value, String gitCommit) {
-    timeout(minutes) {
-        finished = false
-        waitUntil {
-            try {
-                pod = openshift.selector("pod", [(label): value])
-                def podGitCommit = openshift.rsh("${pod.name()} git rev-parse HEAD").out.trim()
-                // Once a new code appeared in a pod we need to wait until it will be fully
-                // deployed.
-                if (podGitCommit == gitCommit) {
-                    echo "Pod (${pod.name()}) with commit hash $podGitCommit has been spawned"
+def call(parameters = [:]) {
+    def cluster = parameters["cluster"]
+    def credentials = parameters["credentials"]
+    def project = parameters["project"]
+    def minutes = parameters["minutes"]
+    def label = parameters["label"]
+    def value = parameters["value"]
+    def gitCommit = parameters["gitCommit"]
+    openshift.withCluster(cluster) {
+        openshift.withCredentials(credentials) {
+            openshift.withProject(project) {
+                timeout(minutes) {
+                    finished = false
                     waitUntil {
-                        // Get the latest replication controller and check its status
-                        def lastRc = openshift.selector("rc", [(label): value]).objects()[-1]
-                        finished = lastRc.status.replicas == lastRc.status.readyReplicas
-                        if (finished) {
-                            echo "Pod (${pod.name()}) has been deployed"
+                        try {
+                            pod = openshift.selector("pod", [(label): value])
+                            def podGitCommit = openshift.rsh("${pod.name()} git rev-parse HEAD").out.trim()
+                            // Once a new code appeared in a pod we need to wait until it will be fully
+                            // deployed.
+                            if (podGitCommit == gitCommit) {
+                                echo "Pod (${pod.name()}) with commit hash $podGitCommit has been spawned"
+                                waitUntil {
+                                    // Get the latest replication controller and check its status
+                                    def lastRc = openshift.selector("rc", [(label): value]).objects()[-1]
+                                    finished = lastRc.status.replicas == lastRc.status.readyReplicas
+                                    if (finished) {
+                                        echo "Pod (${pod.name()}) has been deployed"
+                                    }
+                                    return finished
+                                }
+                            }
+                            return finished
+                        } catch(err) {
+                            echo "Error occured: ${err.getMessage()}"
+                            return false
                         }
-                        return finished
                     }
                 }
-                return finished
-            } catch(any) {
-                return false
             }
         }
     }
