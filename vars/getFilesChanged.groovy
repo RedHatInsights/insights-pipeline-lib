8655fd9f234d537755f067761344c948b992baf5
Brandon Squizzato <bsquizza@redhat.com>
2019-04-10 13:11:29 -0400

    Fix newCommit param get

--- vars/getFilesChanged.groovy
+++ vars/getFilesChanged.groovy
@@ -2,7 +2,7 @@ def call(parameters = [:]) {
     // Check out a repo and then return a list of which files/folders changed between oldCommit and newCommit
     repoDir = parameters['repoDir']
     oldCommit = parameters['oldCommit']
-    newCommit = parameters.get['newCommit']
+    newCommit = parameters['newCommit']
 
     dir(repoDir) {
         sh "git config --add remote.origin.fetch +refs/heads/*:refs/remotes/origin/*"
