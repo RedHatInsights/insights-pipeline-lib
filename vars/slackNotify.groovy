0b8b43994582ab503341d79b777468331ee9ff92
esakaiev <esakaiev@redhat.com>
2019-07-17 21:25:19 +0300

    Adding slackNotify pipeline function

---
+++ vars/slackNotify.groovy
@@ -0,0 +1,31 @@
+/**
+ * Library to slackNotify execute status in Slack
+ * @param stage - stage can be specified here if some exception happened
+ * @param channel - name of slack channel
+ * @param message - message
+ * @param baseUrl - slack baseUrl
+ * @return
+ * to run this method successfully on your Jenkins master need to configure
+ * Global Slack Notifier Settings in jenkins -> Configure
+ * and have installed "Slack Notification Plugin" on your Jenkins master
+ */
+def call(Map parameters = [:]){
+    def stage = parameters.get("stage", "default_stage")
+	def channel = parameters.get("channel", "insights-qe-feed")
+    def message = parameters.get("message", "")
+    def baseUrl = parameters.get("baseUrl", "https://ansible.slack.com/services/hooks/jenkins-ci/")
+
+    def color_map = ['SUCCESS': 'good', 'FAILURE': 'danger', 'UNSTABLE': 'warning', 'ABORTED': 'danger']
+    def default_message = ['SUCCESS': "tests have passed",
+                           'FAILURE': "Failed! OOPS, Something wrong with pipeline",
+                           'UNSTABLE': "tests have failed",
+                           'ABORTED': "Current build have been aborted"]
+
+    slackSend ( baseUrl: baseUrl,
+                botUser: true,
+                channel: channel,
+                color: color_map[currentBuild.currentResult],
+                message: "${message}; ${default_message[currentBuild.currentResult]}; " +
+                         "stage: ${stage};  " +
+                         "build: ${env.BUILD_URL}")
+}
