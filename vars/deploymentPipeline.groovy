7c41c1e28fab65529c25464e6964e28b5cb50fb8
Brandon Squizzato <bsquizza@redhat.com>
2019-09-16 16:46:09 -0400

    Add slackSend to deployment pipeline code

--- vars/deploymentPipeline.groovy
+++ vars/deploymentPipeline.groovy
@@ -59,30 +59,8 @@ private def parseParams(envs, svcs) {
     return [envConfig: envs[params.ENV], imagesToCopy: imagesToCopy, servicesToSkip: servicesToSkip, deployServices: envs[params.ENV]['deployServices']]
 }
 
-// Create a deployment pipeline job given an environment and service config
-def call(p = [:]) {
-    envs = p['environments']
-    svcs = p['services']
-    extraParams = p['extraParams'] ?: []
 
-    properties([parameters(getJobParams(envs, svcs) + extraParams)])
-    parsed = parseParams(envs, svcs)
-
-    // Exit the job if the "reload" box was checked
-    if (params.RELOAD) {
-        echo "Job is only reloading"
-        currentBuild.description = "reload"
-        return
-    }
-
-    // For build #1, only load the pipeline and exit
-    // This is so the next time the job is built, "Build with parameters" will be available
-    if (env.BUILD_NUMBER.toString() == "1") {
-        echo "Initial run, loaded pipeline job and now exiting."
-        currentBuild.description = "loaded params"
-        return
-    }
-
+def runDeploy(parsed) {
     imagesToCopy = parsed['imagesToCopy']
     servicesToSkip = parsed['servicesToSkip']
     envConfig = parsed['envConfig']
@@ -126,4 +104,55 @@ def call(p = [:]) {
             }
         }
     }
+}
+
+
+def sendSlackMsg(msg, color = null) {
+    if (slackChannel && slackUrl) {
+        slackSend(
+            baseUrl: slackUrl,
+            botUser: true,
+            channel: slackChannel,
+            color: color,
+            message: msg
+        )
+    }
+}
+
+
+// Create a deployment pipeline job given an environment and service config
+def call(p = [:]) {
+    envs = p['environments']
+    svcs = p['services']
+    extraParams = p['extraParams'] ?: []
+    slackChannel = p.get('slackChannel')
+    slackUrl = p.get('slackUrl')
+
+    properties([parameters(getJobParams(envs, svcs) + extraParams)])
+    parsed = parseParams(envs, svcs)
+
+    // Exit the job if the "reload" box was checked
+    if (params.RELOAD) {
+        echo "Job is only reloading"
+        currentBuild.description = "reload"
+        return
+    }
+
+    // For build #1, only load the pipeline and exit
+    // This is so the next time the job is built, "Build with parameters" will be available
+    if (env.BUILD_NUMBER.toString() == "1") {
+        echo "Initial run, loaded pipeline job and now exiting."
+        currentBuild.description = "loaded params"
+        return
+    }
+
+    sendSlackMsg("${env.JOB_NAME} started for env ${parsed['envConfig']['env']}")
+
+    try {
+        runDeploy(parsed)
+    } catch (err) {
+        sendSlackMsg("${env.JOB_NAME} failed for env ${parsed['envConfig']['env']} -- see ${env.RUN_DISPLAY_URL}", "danger")
+        throw err
+    }
+    sendSlackMsg("${env.JOB_NAME} successful for env ${parsed['envConfig']['env']}", "good")
 }
