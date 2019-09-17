19f24ee4603f940cbe7f03f03fc15ebb460172f7
Víctor M. Múgica <vmugicag@redhat.com>
2019-09-18 00:13:05 +0200

    Allow priorBuildsCancelled to be run more than once

--- vars/cancelPriorBuilds.groovy
+++ vars/cancelPriorBuilds.groovy
@@ -1,6 +1,20 @@
+import groovy.transform.Field
+
+@Field
+def priorBuildsCancelled = false
+
 def call() {
-    milestone()
-    def buildNumber = env.BUILD_NUMBER as int
-    if (buildNumber > 1) milestone(buildNumber - 1)
-    milestone(buildNumber)
+
+    if (! priorBuildsCancelled) {
+
+        def buildNumber = env.BUILD_NUMBER as int
+
+        if (buildNumber > 1) {
+            milestone(buildNumber - 1)
+        }
+
+        milestone(buildNumber)
+
+        priorBuildsCancelled = true
+    }
 }
