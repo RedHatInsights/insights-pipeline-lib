764cf2dd994a9996c0841b9824e1405d3e9c9ac4
Brandon Squizzato <bsquizza@redhat.com>
2020-01-23 14:37:51 -0500

    Fix src/dst tag quay copy

--- vars/execQuayCopy.groovy
+++ vars/execQuayCopy.groovy
@@ -2,6 +2,7 @@ def call(parameters = [:]) {
     def imageName = parameters['imageName']  // required param
     def imageTag = parameters['imageTag']  // required param
     def dstImageName = parameters.get('dstImageName', imageName)
+    def dstImageTag = parameters.get('dstImageTag', imageTag)
     def jnlpImage = parameters.get('jnlpImage', "jenkins-deploy-jnlp:latest")
     def srcNamespace = parameters.get('srcNamespace', "buildfactory")
     def srcBaseUri = parameters.get(
@@ -18,10 +19,10 @@ def call(parameters = [:]) {
     stage("Start jnlp container") {
         openShiftUtils.withJnlpNode(image: jnlpImage) {
             stage("Copy images") {
-                def isTag = imageName + ":" + imageTag
+                def srcIsTag = imageName + ":" + imageTag
                 def commitId = sh(
                     script: (
-                        "oc describe istag ${isTag} -n ${srcNamespace}" +
+                        "oc describe istag ${srcIsTag} -n ${srcNamespace}" +
                         "| grep ${commitLabel} | cut -f2 -d'='"
                     ),
                     returnStdout: true
@@ -29,13 +30,13 @@ def call(parameters = [:]) {
                 // trim commit hash to 7 chars
                 commitId = commitId[0..6]
                 def commitIsTag = imageName + ":" + commitId
-                sh("oc tag ${isTag} ${commitIsTag} -n ${srcNamespace}")
+                sh("oc tag ${srcIsTag} ${commitIsTag} -n ${srcNamespace}")
 
-                def tags = [dstImageName + ":" + imageTag, dstImageName + ":" + commitId]
-                tags.each { tag ->
+                def tags = [dstImageName + ":" + dstImageTag, dstImageName + ":" + commitId]
+                tags.each { dstTag ->
                     deployUtils.skopeoCopy(
-                        srcUri: srcBaseUri + tag,
-                        dstUri: dstBaseUri + tag,
+                        srcUri: srcBaseUri + srcIsTag,
+                        dstUri: dstBaseUri + dstTag,
                         srcUser: "na",
                         srcTokenId: srcTokenId,
                         dstUser: dstUser,
