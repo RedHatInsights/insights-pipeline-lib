dbe763ee3030387a33877ae79cc25f3b4908ee58
Brandon Schneider <bschneid@redhat.com>
2020-08-26 11:17:42 -0500

    Only trigger the success notification after two successful runs (#94)
    Only trigger the success notification after two successful runs

--- vars/execIQETestsWithNotifier.groovy
+++ vars/execIQETestsWithNotifier.groovy
@@ -31,11 +31,16 @@ def call(args = [:]) {
     // OPTIONAL: closure to call that generates detailed slack msg text when tests fail
     def slackMsgCallback = args.get('slackMsgCallback', defaultSlackMsgCallback)
 
-    def previousBuild = pipelineUtils.getLastRealBuild(currentBuild.getPreviousBuild())
-    if (previousBuild) {
-        echo "Found previous non-RELOAD/non-ERROR build: ${previousBuild.getDisplayName()}"
+    def currentMinusOne = pipelineUtils.getLastRealBuild(currentBuild)
+    if (currentMinusOne) {
+        echo "Found currentMinusOne non-RELOAD/non-ERROR build: ${currentMinusOne.getDisplayName()}"
     }
-
+        
+    def currentMinusTwo = pipelineUtils.getLastRealBuild(currentMinusOne)
+    if (currentMinusTwo) {
+        echo "Found currentMinusTwo non-RELOAD/non-ERROR build: ${currentMinusTwo.getDisplayName()}"
+    }
+        
     def results
     try {
         results = execIQETests(
@@ -52,7 +57,7 @@ def call(args = [:]) {
         if (!results) error("Found no test results, unexpected error must have occurred")
 
         if (results['failed']) {
-            if (!previousBuild || previousBuild.getResult().toString() == "SUCCESS") {
+            if (!currentMinusOne || currentMinusOne.getResult().toString() == "SUCCESS") {
                 // result went from success -> failed
                 // run script to collect request ID info and send the failure slack msg
                 def slackMsg = slackMsgCallback()
@@ -64,8 +69,8 @@ def call(args = [:]) {
                 )
             }
         }
-        else if (previousBuild && previousBuild.getResult().toString() != "SUCCESS") {
-            // result went from failed -> success
+        else if (currentMinusOne && currentMinusTwo && currentMinusOne.getResult().toString() == "SUCCESS" && currentMinusTwo.getResult().toString() != "SUCCESS") {
+            // result went from failed -> success -> success
             slackUtils.sendMsg(
                 slackChannel: slackChannel,
                 slackUrl: slackUrl,
--- vars/pipelineUtils.groovy
+++ vars/pipelineUtils.groovy
@@ -232,6 +232,8 @@ def throwIfAborted(Exception err) {
 def getLastRealBuild(build) {
     // find the first previous build that is not a 'reload' or 'error' build
     if (build == null) return null
-    if (build.description != "reload" && build.description != "error") return build
-    else return getLastRealBuild(build.getPreviousBuild())
+    def previousBuild = build.getPreviousBuild()
+    if (previousBuild == null) return null
+    if (previousBuild.description != "reload" && previousBuild.description != "error") return previousBuild
+    else return getLastRealBuild(previousBuild)
 }
