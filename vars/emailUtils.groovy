c227bbf71b7d5fc3a6c3900adca77204ca8a2b7d
Mohan Seri <mseri@redhat.com>
2020-09-15 09:08:57 -0600

    Email utils

---
+++ vars/emailUtils.groovy
@@ -0,0 +1,20 @@
+/**
+ * Requires "Email Notifications Plugin"
+ */
+import groovy.transform.Field
+
+
+def sendEmail(parameters = [:]) {
+    def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
+    def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
+    def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
+    def content = parameters.get('content', pipelineVars.emailDefaultContent)
+
+
+    if(sentTo != null && !sentTo.isEmpty()) {
+      emailext(body: content, mimeType: 'text/html',
+         replyTo: '$DEFAULT_REPLYTO', subject: subject,
+         sentTo: sentTo, attachLog: true )
+    }
+}
+
