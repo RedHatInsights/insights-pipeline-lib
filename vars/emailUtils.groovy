36c4b3c509a6d274d0d38cfcea441fa7e05642b3
Mohan Seri <mseri@redhat.com>
2020-09-15 16:19:39 -0600

    email utils

--- vars/emailUtils.groovy
+++ vars/emailUtils.groovy
@@ -4,12 +4,23 @@
 import groovy.transform.Field
 
 
-def sendEmail(parameters = [:]) {
+// private def sendEmail(parameters = [:]) {
+//
+//         def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
+//         def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
+//         def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
+//         def content = parameters.get('content', pipelineVars.emailDefaultContent)
+//
+//         println "sending to: $sentTo"
+//
+//         if(sentTo != null && !sentTo.isEmpty()) {
+//           emailext(body: content, mimeType: 'text/html',
+//              replyTo: '$DEFAULT_REPLYTO', subject: subject,
+//              to: sentTo, attachLog: true )
+//         }
+// }
 
-        def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
-        def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
-        def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
-        def content = parameters.get('content', pipelineVars.emailDefaultContent)
+private def sendEmail(sentTo, replyTo, subject, content) {
 
         println "sending to: $sentTo"
 
@@ -18,5 +29,15 @@ def sendEmail(parameters = [:]) {
              replyTo: '$DEFAULT_REPLYTO', subject: subject,
              to: sentTo, attachLog: true )
         }
+}
+
+
+def call(parameters = [:]) {
+        def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
+        def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
+        def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
+        def content = parameters.get('content', pipelineVars.emailDefaultContent)
+
+        sendEmail(sentTo, replyTo, subject, content)
 }
 
