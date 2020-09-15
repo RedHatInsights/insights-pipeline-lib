44669a446df9781879a6de233870c23996c99abb
Mohan Seri <mseri@redhat.com>
2020-09-15 10:53:43 -0600

    email utils

--- vars/emailUtils.groovy
+++ vars/emailUtils.groovy
@@ -5,19 +5,22 @@ import groovy.transform.Field
 
 
 def sendEmail(parameters = [:]) {
-    def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
-    def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
-    def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
-    def content = parameters.get('content', pipelineVars.emailDefaultContent)
 
-    println "sending to: $sentTo"
+    node {
+        def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
+        def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
+        def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
+        def content = parameters.get('content', pipelineVars.emailDefaultContent)
+
+        println "sending to: $sentTo"
 
 
 
-    if(sentTo != null && !sentTo.isEmpty()) {
-      emailext(body: content, mimeType: 'text/html',
-         replyTo: '$DEFAULT_REPLYTO', subject: subject,
-         to: sentTo, attachLog: true )
+        if(sentTo != null && !sentTo.isEmpty()) {
+          emailext(body: content, mimeType: 'text/html',
+             replyTo: '$DEFAULT_REPLYTO', subject: subject,
+             to: sentTo, attachLog: true )
+        }
     }
 }
 
