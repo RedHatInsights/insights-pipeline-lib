acb4b739555ffcda81e47fea8c0b136a3a140c36
Mohan Seri <mseri@redhat.com>
2020-09-16 13:08:59 -0600

    Changes made as per review

--- vars/emailUtils.groovy
+++ vars/emailUtils.groovy
@@ -2,26 +2,16 @@
  * Requires "Email Notifications Plugin"
  */
 
-private def sendEmail(sentTo, replyTo, subject, content) {
+def sendEmail(args = [:]) {
+    def to = args.get('to')
+    def replyTo = args.get('replyTo')
+    def subject = args.get('subject')
+    def content = args.get('content')
 
-        if(sentTo != null && !sentTo.isEmpty()) {
-          emailext(body: content, mimeType: 'text/html',
-             replyTo: '$DEFAULT_REPLYTO', subject: subject,
-             to: sentTo, attachLog: true )
-        }
+    if(to != null && !to.isEmpty()) {
+      emailext(body: content, mimeType: 'text/html',
+         replyTo: replyTo, subject: subject,
+         to: to, attachLog: true )
+    }
 }
 
-
-def call(parameters = [:]) {
-        def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
-        def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
-        def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
-        def content = parameters.get('content', pipelineVars.emailDefaultContent)
-        def extraJobProperties = parameters.get('extraJobProperties', [])
-
-        def jobProperties = []
-        jobProperties.addAll(extraJobProperties)
-        properties(jobProperties)
-
-        sendEmail(sentTo, replyTo, subject, content)
-}
