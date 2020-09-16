ec441816f5779bc079083a30f04c118282b78333
Mohan Seri <mseri@redhat.com>
2020-09-15 23:34:16 -0600

    email utils

--- vars/emailUtils.groovy
+++ vars/emailUtils.groovy
@@ -1,20 +1,33 @@
 /**
  * Requires "Email Notifications Plugin"
  */
-//
 import groovy.transform.Field
-// import hudson.AbortException
+
+
+// private def sendEmail(parameters = [:]) {
+//
+//         def sentTo = parameters.get('sentTo', pipelineVars.emailDefaultSentTo)
+//         def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
+//         def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
+//         def content = parameters.get('content', pipelineVars.emailDefaultContent)
+//
+//         println "sending to: $sentTo"
+//
+//         if(sentTo != null && !sentTo.isEmpty()) {
+//           emailext(body: content, mimeType: 'text/html',
+//              replyTo: '$DEFAULT_REPLYTO', subject: subject,
+//              to: sentTo, attachLog: true )
+//         }
+// }
 
 private def sendEmail(sentTo, replyTo, subject, content) {
+
+        println "sending to: $sentTo"
+
         if(sentTo != null && !sentTo.isEmpty()) {
-          emailext(
-            to: sentTo,
-            subject: subject,
-            body: content,
-            mimeType: 'text/html',
-            replyTo: replyTo,
-            attachLog: true
-          )
+          emailext(body: content, mimeType: 'text/html',
+             replyTo: '$DEFAULT_REPLYTO', subject: subject,
+             to: sentTo, attachLog: true )
         }
 }
 
@@ -24,12 +37,6 @@ def call(parameters = [:]) {
         def replyTo = parameters.get('replyTo', pipelineVars.emailDefaultReplyTo)
         def subject = parameters.get('subject', pipelineVars.emailDefaultSubject)
         def content = parameters.get('content', pipelineVars.emailDefaultContent)
-//         def extraJobProperties = parameters.get('extraJobProperties', [])
-//
-//         def jobProperties = []
-//         jobProperties.addAll(extraJobProperties)
-//         properties(jobProperties)
 
-        sendEmail(sentTo, replyTo, subject, content, replyTo)
+        sendEmail(sentTo, replyTo, subject, content)
 }
-
