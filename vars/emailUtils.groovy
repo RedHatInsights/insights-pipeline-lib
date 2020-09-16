28a342cad75054f780db8e06e60b1e0275b9d5ca
Mohan Seri <mseri@redhat.com>
2020-09-16 12:46:07 -0600

    email utils

--- vars/emailUtils.groovy
+++ vars/emailUtils.groovy
@@ -3,10 +3,10 @@
  */
 
 def sendEmail(args = [:]) {
-        def to = args.get('to', pipelineVars.emailDefaultTo)
-        def replyTo = args.get('replyTo', pipelineVars.emailDefaultReplyTo)
-        def subject = args.get('subject', pipelineVars.emailDefaultSubject)
-        def content = args.get('content', pipelineVars.emailDefaultContent)
+        def to = args.get('to')
+        def replyTo = args.get('replyTo')
+        def subject = args.get('subject')
+        def content = args.get('content')
 
         if(to != null && !to.isEmpty()) {
           emailext(body: content, mimeType: 'text/html',
