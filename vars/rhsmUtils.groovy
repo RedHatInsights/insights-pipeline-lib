27d7d4391df2a97f5fa464f8c2950eca5eb17d68
ecerquei <ecerquei@redhat.com>
2020-10-28 13:16:12 -0400

    RHSM functions

--- vars/rhsmUtils.groovy
+++ vars/rhsmUtils.groovy
@@ -1,32 +1,56 @@
 /*
-* Subscribe RHEL to RHSM
-* @param url
-* @param credentialId from Jenkins credential
-* @param poolId (optional)
+* RHSM register
+* @param (optional) url = RHSM url
+* @param credentialId = Jenkins credential id to authenticate using username and password
+* @param (optional) poolId = RHSM pool id
+* @param (optional) activationKey = RHSM activation key, usually used for Satellite hosts
+* @param (optional) org = Satellite Organization name
 */
-def register(String url, String credentialId, String poolId=null){
-    withCredentials([usernamePassword(credentialsId: credentialId, usernameVariable: 'username', passwordVariable: 'password')]) {
-        sh """
-            subscription-manager register --serverurl=${url} --username=${username} --password=${password}
-        """
-    }
+def register(
+        String url=null, 
+        String credentialId, 
+        String poolId=null, 
+        String activiationKey=null, 
+        String org=null){
     if(poolId){
-        sh """
-            subscription-manager attach --pool=${poolId}
-        """
+        withCredentials([usernamePassword(credentialsId: credentialId, usernameVariable: 'username', passwordVariable: 'password')]) {
+            sh """
+                subscription-manager register --serverurl=${url} --username=${username} --password=${password}
+                subscription-manager attach --pool=${poolId}
+                subscription-manager refresh
+            """
+        }
+    }
+    else if (activiationKey){
+        withCredentials([usernamePassword(credentialsId: credentialId, usernameVariable: 'username', passwordVariable: 'password')]) {
+            sh """
+                subscription-manager register --org=${org} --activationkey=${activiationKey}
+                subscription-manager refresh
+            """
+        }
     }
     else {
-        sh """
-            subscription-manager --auto-attach --force
-        """
+        withCredentials([usernamePassword(credentialsId: credentialId, usernameVariable: 'username', passwordVariable: 'password')]) {
+            sh """
+                subscription-manager register --serverurl=${url} --username=${username} --password=${password} --auto-attach --force
+                subscription-manager refresh
+            """
+        }
     }
 }
 
-// Unsubscribe RHEL system from RHSM
 def unregister(){
     sh '''
         subscription-manager remove --all
         subscription-manager unregister
         subscription-manager clean
     '''
+}
+
+def list(){
+    sh "subscription-manager list --available"
+}
+
+def status(){
+    sh "subscription-manager status"
 }
