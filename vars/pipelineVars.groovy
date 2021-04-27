24887a0d71a93ff8cb203855697f55cbb3bd5757
Dmitry Misharov <dmisharo@redhat.com>
2021-04-27 08:43:44 +0200

    Bump browsers versions

--- vars/pipelineVars.groovy
+++ vars/pipelineVars.groovy
@@ -35,7 +35,7 @@ class pipelineVars implements Serializable {
     )
     String iqeCoreImage = 'quay.io/cloudservices/iqe-core:latest'
     String iqeTestsImage = 'quay.io/cloudservices/iqe-tests:latest'
-    String seleniumImage = 'quay.io/redhatqe/selenium-standalone:ff_78.6.1esr_gecko_v0.29.0_chrome_88.0.4324.96'
+    String seleniumImage = 'quay.io/redhatqe/selenium-standalone:ff_78.9.0esr_gecko_v0.29.1_chrome_89.0.4389.128'
 
     String defaultCloud = 'openshift'
     String defaultNamespace = 'jenkins'
