ad1a9379c6128dbed9db5ac7e5dcb8507fb2f7cc
Christian <ctorrens@redhat.com>
2022-10-18 21:28:57 +0200

    galaxy_ng stage tests pipeline

---
+++ vars/galaxyNgStageTests.groovy
@@ -0,0 +1,39 @@
+
+def call(args = [:]) {
+pipeline {
+        parameters {
+            password(name: 'TOKEN', defaultValue: 'secret', description: 'Github token')
+        }
+
+        stages {
+            stage('Checkout galaxy_ng repo') {
+                steps {
+                    gitUtils.checkOutRepo(
+                        targetDir: "galaxy_ng",
+                        repoUrl: "git@github.com:ansible/galaxy_ng.git",
+                        credentialsId: "InsightsDroidGitHubHTTP",
+                        branch: "cloud_tests"
+                    )
+                }
+            }
+
+            stage('Run') {
+                steps {
+                    script {
+                        sh "IQE_VAULT_GITHUB_TOKEN=${params.TOKEN} ./galaxy_ng/dev/common/RUN_INTEGRATION_STAGE.sh"
+                    }
+                }
+            }
+        }
+
+        post {
+            always {
+                script{
+                    archiveArtifacts allowEmptyArchive: true, artifacts: 'galaxy_ng-results.xml'
+                    junit allowEmptyResults: true, testResults: 'galaxy_ng-results.xml'
+                }
+            }
+            
+        }
+    }
+}
