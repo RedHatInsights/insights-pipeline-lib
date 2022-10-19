d5ca217a88f3eb58b24b75e15fe39db571a1fc43
Christian <ctorrens@redhat.com>
2022-10-19 11:02:02 +0200

    setupVaultParameters

--- vars/galaxyNgStageTests.groovy
+++ vars/galaxyNgStageTests.groovy
@@ -1,29 +1,26 @@
 
 def call(args = [:]) {
-
-node    {
-        parameters {
-            password(name: 'TOKEN', defaultValue: 'secret', description: 'Github token')
+    node {
+        stage('Checkout galaxy_ng repo') {
+            gitUtils.checkOutRepo(
+                targetDir: "galaxy_ng",
+                repoUrl: "https://github.com/ansible/galaxy_ng.git",
+                credentialsId: "InsightsDroidGitHubHTTP",
+                branch: "cloud_tests"
+            )
         }
+        stage('Run') {
+            // vaultRoleId, vaultSecretId
+            // params['vaultRoleIdCredential'] = pipelineVars.defaultVaultRoleIdCredential
+            // params['vaultSecretIdCredential'] = pipelineVars.defaultVaultSecretIdCredential
 
-            stage('Checkout galaxy_ng repo') {
-                    gitUtils.checkOutRepo(
-                        targetDir: "galaxy_ng",
-                        repoUrl: "https://github.com/ansible/galaxy_ng.git",
-                        credentialsId: "InsightsDroidGitHubHTTP",
-                        branch: "cloud_tests"
-                    )
-                
+            vaultParameters = setupVaultParameters()
+            iqeUtils.writeVaultEnvVars(vaultParameters)
+            script {
+                // sh "IQE_VAULT_GITHUB_TOKEN=${params.TOKEN} ./galaxy_ng/dev/common/RUN_INTEGRATION_STAGE.sh"
+                sh "./galaxy_ng/dev/common/RUN_INTEGRATION_STAGE.sh"
             }
-
-            stage('Run') {
-                    script {
-                        sh "IQE_VAULT_GITHUB_TOKEN=${params.TOKEN} ./galaxy_ng/dev/common/RUN_INTEGRATION_STAGE.sh"
-                    }
-                
-            }
-        
-
+        }
         post {
             always {
                 script{
@@ -31,8 +28,6 @@ node    {
                     junit allowEmptyResults: true, testResults: 'galaxy_ng-results.xml'
                 }
             }
-            
         }
     }
-    
 }
