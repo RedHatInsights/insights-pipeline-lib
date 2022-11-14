5d08859a7a5dba6d79e18a61a8c84d434ae99e13
Christian <ctorrens@redhat.com>
2022-11-14 10:22:48 +0100

    python-39

--- vars/galaxyNgStageTests.groovy
+++ vars/galaxyNgStageTests.groovy
@@ -1,24 +1,20 @@
 def call(args = [:]) {
-        openShiftUtils.withNode(image: "centos/python-36-centos7") {
-            stage('Checkout galaxy_ng repo') {
-                container('galaxy'){
-                    gitUtils.checkOutRepo(
-                        targetDir: "galaxy_ng",
-                        repoUrl: "https://github.com/ansible/galaxy_ng.git",
-                        credentialsId: "InsightsDroidGitHubHTTP",
-                        branch: "master"
-                    )
-                }
-            }
-            stage('Run integration tests against stage') {
-                container('galaxy'){
-                    withCredentials([ string(credentialsId: 'vaultRoleId', variable: 'IQE_VAULT_ROLE_ID'), string(credentialsId: 'vaultSecretId', variable: 'IQE_VAULT_SECRET_ID')])
-                    {
-                        sh ( script: "IQE_VAULT_ROLE_ID=${IQE_VAULT_ROLE_ID} IQE_VAULT_SECRET_ID=${IQE_VAULT_SECRET_ID} ./galaxy_ng/dev/common/RUN_INTEGRATION_STAGE.sh", returnStatus: true)
-                        archiveArtifacts allowEmptyArchive: true, artifacts: 'galaxy_ng-results.xml'
-                        junit allowEmptyResults: true, testResults: 'galaxy_ng-results.xml'
-                    }
-                }
+    openShiftUtils.withNode((image: "registry.access.redhat.com/ubi8/python-39")) {
+        stage('Checkout galaxy_ng repo') {
+            gitUtils.checkOutRepo(
+                targetDir: "galaxy_ng",
+                repoUrl: "https://github.com/ansible/galaxy_ng.git",
+                credentialsId: "InsightsDroidGitHubHTTP",
+                branch: "master"
+            )
+        }
+        stage('Run integration tests against stage') {
+            withCredentials([ string(credentialsId: 'vaultRoleId', variable: 'IQE_VAULT_ROLE_ID'), string(credentialsId: 'vaultSecretId', variable: 'IQE_VAULT_SECRET_ID')])
+            {
+                sh ( script: "IQE_VAULT_ROLE_ID=${IQE_VAULT_ROLE_ID} IQE_VAULT_SECRET_ID=${IQE_VAULT_SECRET_ID} ./galaxy_ng/dev/common/RUN_INTEGRATION_STAGE.sh", returnStatus: true)
+                archiveArtifacts allowEmptyArchive: true, artifacts: 'galaxy_ng-results.xml'
+                junit allowEmptyResults: true, testResults: 'galaxy_ng-results.xml'
             }
         }
+    }
 }
