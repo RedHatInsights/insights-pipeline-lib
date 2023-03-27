eb8d940737b56caa13254306e2d334feb3dea634
Brandon Squizzato <bsquizza@redhat.com>
2023-03-27 10:07:29 -0400

    Update default slack URL

--- vars/pipelineVars.groovy
+++ vars/pipelineVars.groovy
@@ -44,10 +44,10 @@ class pipelineVars implements Serializable {
     String upshiftCloud = 'upshift'
     String upshiftNameSpace = 'insights-qe-ci'
 
-    String slackDefaultUrl = 'https://ansible.slack.com/services/hooks/jenkins-ci/'
+    String slackDefaultUrl = "https://redhat-internal.slack.com/services/hooks/jenkins-ci/"
     String slackDefaultChannel = '#insights-qe-feed'
     String slackDeployAlertsChannel = "#deploy-alerts"
-    String slackDefaultTeamDomain = "ansible"
+    String slackDefaultTeamDomain = "redhat-internal"
 
     String quayBaseUri = 'quay.io/cloudservices'
     String quayUser = "cloudservices+push"
