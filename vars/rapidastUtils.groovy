90c799c709fc65462743596b4ecd0d15ae3ad431
Sanket Jagtap <sjagtap@redhat.com>
2023-09-05 14:03:59 +0530

    rapidast points to latest tag and points to main branch

--- vars/rapidastUtils.groovy
+++ vars/rapidastUtils.groovy
@@ -2,7 +2,7 @@
 
 
 def prepareRapidastStages(String ServiceName, String ApiScanner, String TargetUrl, String ApISpecUrl) {
-    openShiftUtils.withNode(cloud: pipelineVars.upshiftCloud, image: 'quay.io/insights-qe/rapidast:multiuser') {
+    openShiftUtils.withNode(cloud: pipelineVars.upshiftCloud, image: 'quay.io/redhatproductsecurity/rapidast:latest') {
 
         stage("Set Build Rapidast for ${ServiceName} service") {
              currentBuild.displayName = "#"+ env.BUILD_NUMBER + " " + "${ServiceName}"
@@ -60,27 +60,40 @@ def prepareRapidastStages(String ServiceName, String ApiScanner, String TargetUr
 def parse_rapidast_options(String ServiceName, String ApiScanner, String TargetUrl, String ApISpecUrl) {
     // Parse the options for rapidast and add it to the config file. Always pull the latest config file
 
-    git url: 'https://github.com/RedHatProductSecurity/rapidast.git', branch: 'development'
+    git url: 'https://github.com/RedHatProductSecurity/rapidast.git', branch: 'main'
     def filename = 'config/config-template-long.yaml'
 
     // Comment the fields not required.
     sh "sed -i 's/importUrlsFromFile:/# importUrlsFromFile:/' ${filename}"
     sh "sed -i 's/defectDojoExport:/# defectDojoExport:/' ${filename}"
     sh "sed -i 's/# format:/format:/' ${filename}"
-    if ("${ApiScanner}" == "OpenApiScan") {
-        // Comment the fields not required.
+    sh "sed -i 's/spiderAjax:/# spiderAjax:/' ${filename}"
+    sh "sed -i 's/spider:/# spider:/' ${filename}"
+    sh "sed -i 's/maxDuration:/# maxDuration:/g' ${filename}"
+    sh "sed -i 's/browserId:/# browserId:/' ${filename}"
+    sh "sed -i 's/url:/# url:/g' ${filename}"
+    if ("${ApiScanner}" == "apiScan") {
         echo "OpenAPI Spec Compliant API Scan selected"
         sh "sed -i 's/graphql:/# graphql:/' ${filename}"
         sh "sed -i 's/spiderAjax:/# spiderAjax:/' ${filename}"
         sh "sed -i 's/spider:/# spider:/' ${filename}"
+        data = readYaml file: filename
+        data.scanners.zap.apiScan.target = "${TargetUrl}"
+        data.scanners.zap.apiScan.apis.apiUrl = "${OpenApISpecUrl}"
+    }
+    else if ("${ApiScanner} == graphql") {
+        sh "sed -i 's/apiScan:/# apiScan:/' ${filename}"
+        sh "sed -i 's/target:/# target:/' ${filename}"
+        sh "sed -i 's/apis:/# apis:/' ${filename}"
+        sh "sed -i 's/apiUrl:/# apiUrl:/' ${filename}"
+        sh "sed -i 's/# schemaUrl:/schemaUrl:/' ${filename}"
+        data = readYaml file: filename
+        data.scanners.zap.graphql.endpoint = "${TargetUrl}"
+        data.scanners.zap.graphql.schemaUrl = "${OpenApISpecUrl}"
     }
     else {
-        echo "Scanner not supported."
-        currentBuild.result = 'FAILURE'
-        sh "exit 1"
+        echo "Scanner not supported"
     }
-    // Read the YAML and them populate the fields
-    def data = readYaml file: filename
     data.config.environ = ".env"
     data.application.shortName = "${ServiceName}"
     data.application.url = "${TargetUrl}"
@@ -89,10 +102,12 @@ def parse_rapidast_options(String ServiceName, String ApiScanner, String TargetU
     data.general.authentication.parameters.client_id = "rhsm-api"
     data.general.authentication.parameters.token_endpoint = pipelineVars.stageSSOUrl
     data.general.container.type = "none"
-    data.scanners.zap.apiScan.target = "${TargetUrl}"
-    data.scanners.zap.apiScan.apis.apiUrl = "${ApISpecUrl}"
-    data.scanners.zap.miscOptions.oauth2OpenapiManualDownload = "True"
-
+    try {
+        data.scanners.zap.miscOptions.oauth2OpenapiManualDownload = "True"
+    } catch(Exception e) {
+        println("Exception: ${e}")
+        data.scanners.zap.miscOptions.oauth2ManualDownload = "True"
+    }
     //create new with updated YAML config
     writeYaml file: 'config/config.yaml', data: data
     echo "Configuration Value: " + data
